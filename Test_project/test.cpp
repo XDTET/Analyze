d3ac91ff53804e4886d15545cf30562ec80c3b21
Some One <adrian97.z@outlook.com>
2022-01-23 18:55:54 +0100

    changed path

--- Test_project/test.cpp
+++ Test_project/test.cpp
@@ -4,6 +4,8 @@
 
 threadPools pools(std::thread::hardware_concurrency());
 
+std::string path = R"(C:\Users\Adrian\Documents\Nowyfolder)";
+
 TEST(EmptyTaskShouldReturn0, empty)
 {
 	ASSERT_TRUE(pools.get_tasks() == 0);
@@ -11,7 +13,7 @@ TEST(EmptyTaskShouldReturn0, empty)
 
 TEST(GivenTaskShouldHaveOccurence, occurence)
 {
-	listFilesWithThreads(R"(C:\Users\Adrian\Documents\Nowyfolder)");
+	listFilesWithThreads(path);
 
 	int total = pools.get_tasks();
 
@@ -20,28 +22,28 @@ TEST(GivenTaskShouldHaveOccurence, occurence)
 
 TEST(ValidPathWithDirectoryShouldHaveFilledCounter, filled_counter)
 {
-	countStats(R"(C:\Users\Adrian\Documents\Nowyfolder)");
+	countStats(path);
 	
 	ASSERT_TRUE(nrOf.howManyDirectories > 0);
 }
 
 TEST(ValidPathWithFileShouldHaveFilledCounter, filled_file)
 {
-	countStats(R"(C:\Users\Adrian\Documents\Nowyfolder)");
+	countStats(path);
 
 	ASSERT_TRUE(nrOf.howManyFiles > 0);
 }
 
 TEST(ValidPathWithEmptyLineShouldHaveFilledCounter, filled_line)
 {
-	countStats(R"(C:\Users\Adrian\Documents\Nowyfolder)");
+	countStats(path);
 
 	ASSERT_TRUE(nrOf.emptyLines > 0);
 }
 
 TEST(ValidPathWithNonEmptyLineShouldHaveFilledCounter, filled_empty_line)
 {
-	countStats(R"(C:\Users\Adrian\Documents\Nowyfolder)");
+	countStats(path);
 
 	ASSERT_TRUE(nrOf.nonEmptyLines > 0);
 }
@@ -51,14 +53,14 @@ TEST(NotValidPathShouldHaveEmptyCounter, empty_counter)
 
 	nrOf.howManyDirectories = 0;
 
-	countStats(R"(C:\Users\Adrian\Document)");
+	countStats(path);
 
 	ASSERT_TRUE(nrOf.howManyDirectories == 0);
 }
 
 TEST(ValidPathShouldListDirs, valid_dir)
 {
-	listFilesWithThreads(R"(C:\Users\Adrian\Documents\Nowyfolder)");
+	listFilesWithThreads(path);
 
 	ASSERT_TRUE(nrOf.nonEmptyLines > 0);
 }
@@ -67,7 +69,7 @@ TEST(NotValidPathShouldNotListDirs)
 {
 	nrOf.nonEmptyLines = 0;
 
-	countStats(R"(C:\Users\Adrian\Documents\Nowyfolder)");
+	countStats(path);
 
 	ASSERT_TRUE(nrOf.nonEmptyLines = 0);
 }
